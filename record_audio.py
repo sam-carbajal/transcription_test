55c76fb249a5c43493715100d6f3f507390b5c1d
Sam <samantha.carbajal90@gmail.com>
2026-06-05 16:12:39 +0200

    Download Button

--- record_audio.py
+++ record_audio.py
@@ -67,31 +67,27 @@ audio = mic_recorder(
     key="recorder",
 )
 
-filename = f"{speaker}_{gender}_{group}_{mic_key}"
+filename = f"{speaker}_{gender}_{group}_{mic_key}.wav"
 
 
 if audio:
     st.audio(audio["bytes"])
 
-    audio_data = np.frombuffer(audio["bytes"], dtype=np.int16)
+    with open(filename, "wb") as f:
+        f.write(audio["bytes"])
 
-    write(
-        filename,
-        audio["sample_rate"],
-        audio_data
+
+    #with open(filename, "rb") as f: #Öffnet die gespeicherte Datei im Binärmodus, weil Audio keine Textdatei ist, sondern rohe Bytes enthält
+    st.download_button(
+        label="Download Audio (WAV)",
+        data=f,
+        file_name=filename,
+        mime="audio/wav"
     )
-
-    with open(filename, "rb") as f: #Öffnet die gespeicherte Datei im Binärmodus, weil Audio keine Textdatei ist, sondern rohe Bytes enthält
-        st.download_button(
-            label="Download Audio (WAV)",
-            data=f,
-            file_name=f"{filename}.wav",
-            mime="audio/wav"
-        )
 
     #    st.download_button(
     #        label="Download Audio (WEBM)",
     #        data=audio["bytes"],
-    #        file_name=f"{filename}.webm",
+    #        file_name=filename.replace(".wav", ".webm"),
     #        mime="audio/webm"
     #    )
