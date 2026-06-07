a3eb14765433056258f3081713558f30867a9738
Sam <samantha.carbajal90@gmail.com>
2026-06-07 20:18:44 +0200

    Audio webm

--- record_audio.py
+++ record_audio.py
@@ -83,9 +83,10 @@ def detect_audio_format(audio_bytes):
         return "unknown"
 
 if audio:
-    st.audio(audio["bytes"])
+    #st.audio(audio["bytes"])
     format_detected = detect_audio_format(audio["bytes"])
     st.write("Detected format:", format_detected)
+    st.audio(audio["bytes"], format="audio/webm")
 
     audio_format = 0
     if format_detected == "webm" or format_detected == "mp4/webm":
