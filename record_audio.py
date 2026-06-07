411eaed740bb1eabb42a8f5541b1c23c704c83d4
Sam <samantha.carbajal90@gmail.com>
2026-06-07 20:05:48 +0200

    Änderung von Audio Format, Audio Format selbst entscheidend

--- record_audio.py
+++ record_audio.py
@@ -72,12 +72,13 @@ audio = mic_recorder(
     key="recorder",
 )
 
-filename = f"{speaker_key}_{gender_key}_{group_key}_{mic_key}.webm"
-#Python Code entscheidet nicht das Format, der Browser entscheidet
 
 if audio:
     st.audio(audio["bytes"])
-    st.write(audio)
+    audio_format = audio #st.write(audio) - audio format
+
+    filename = f"{speaker_key}_{gender_key}_{group_key}_{mic_key}.{audio_format}"
+    # Python Code entscheidet nicht das Format, der Browser entscheidet
 
     with open(filename, "wb") as f:
         f.write(audio["bytes"])  #with open(filename, "rb") as f: #Öffnet die gespeicherte Datei im Binärmodus, weil Audio keine Textdatei ist, sondern rohe Bytes enthält
